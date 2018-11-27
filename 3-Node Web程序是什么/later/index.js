67fd72ac2c1fbf4302f31d36456d13ee12d554ce
zhoujin <zhoujin@iboxpay.com>
2018-11-27 16:02:10 +0800

    Node Web

--- 3-Node Web程序是什么/later/index.js
+++ 3-Node Web程序是什么/later/index.js
@@ -1,36 +1,72 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const articles = [{ title: 'Example' }]
-
+const read = require('node-readability')
+const url = 'http://www.manning.com/cantelon2/'
+const Article = require('./db.js').Article
 app.set('port', process.env.PORT || 3000)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(
+    '/css/bootstrap.css',
+    express.static('node_module/bootstrap/dist/css/bootstrap.css')
+)
+
 app.get('/articles', (req, res, next) => {  // 获取所有文字
-    res.send(articles)
+    Article.all((err, articles) => {
+        if (err) {
+            return next(err)
+        }
+        // res.send(articles)
+        res.format({
+            html: () => {
+                res.render('articles.ejs', { article })
+            },
+            json: () => {
+                res.send(articles)
+            }
+        })
+    })
 })
 
 app.post('/articles', (req, res, next) => {  // 创建一篇文章
-    const article = { title: req.body.title }
-    articles.push(article)
-    res.send(articles)
+    const url = req.body.url
+    read(url, (err, result) => {
+        if (err || !result) {
+            res.status(500).send('Error downloading article')
+        } else {
+            Article.create({ title: result.title, content: result.content }, (err, article) => {
+                if (err) {
+                    return next(err)
+                }
+                res.send('OK')
+            })
+        }
+    })
 })
 
 
 
 app.get('/articles/:id', (req, res, next) => { // 获取指定文章
     const id = req.params.id
-    console.log('Fetching:', id)
-    res.send(articles[id])
+    Article.find(id, (err, article) => {
+        if (err) {
+            return next(err)
+        }
+        res.send(article)
+    })
 })
 
 app.delete('/articles/:id', (req, res, next) => { // 删除指定文章
     const id = req.params.id
-    console.log('Deleting:', id)
-    delete articles[id]
-    res.send({ message: 'Deleted' })
+    Article.delete(id, (err) => {
+        if (err) {
+            return next(err)
+        }
+        res.send({ message: 'Deleted' })
+    })
 })
 
 app.listen(app.get('port'), () => {
