9ad9a622e45ca3e7eb3da2bdd4b2d2d207747d7d
zhoujin <zhoujin@iboxpay.com>
2018-11-27 11:18:26 +0800

    express - REST API

---
+++ 3-Node Web程序是什么/later/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const articles = [{ title: 'Example' }]
+
+app.set('port', process.env.PORT || 3000)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/articles', (req, res, next) => {  // 获取所有文字
+    res.send(articles)
+})
+
+app.post('/articles', (req, res, next) => {  // 创建一篇文章
+    const article = { title: req.body.title }
+    articles.push(article)
+    res.send(articles)
+})
+
+
+
+app.get('/articles/:id', (req, res, next) => { // 获取指定文章
+    const id = req.params.id
+    console.log('Fetching:', id)
+    res.send(articles[id])
+})
+
+app.delete('/articles/:id', (req, res, next) => { // 删除指定文章
+    const id = req.params.id
+    console.log('Deleting:', id)
+    delete articles[id]
+    res.send({ message: 'Deleted' })
+})
+
+app.listen(app.get('port'), () => {
+    console.log('App started on port', app.get('port'))
+})
+
+module.exports = app;
+
+
+
