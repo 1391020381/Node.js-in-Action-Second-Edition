e748fbd19bd824742d5e23d19c22ca425b86b195
zhoujin <zhoujin@iboxpay.com>
2018-11-28 16:25:49 +0800

    添加数据到redis中

--- 6-深入了解Connect和Express/express-demo/routes/entries.js
+++ 6-深入了解Connect和Express/express-demo/routes/entries.js
@@ -1,13 +1,13 @@
 
 const Entry = require('../models/entry')
 exports.form = (req, res) => {
-    console.log(req, res)
     res.render('post', { title: 'Post' })
 }
 
 exports.submit = (req, res, next) => {
     const data = req.body.entry
     const user = res.locals.user
+    const username = user ? user.name : null
     const entry = new Entry({
         username: username,
         title: data.title,
@@ -20,3 +20,4 @@ exports.submit = (req, res, next) => {
         res.redirect('/')
     })
 }
+
